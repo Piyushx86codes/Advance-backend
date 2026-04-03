1b5324840dbef27c9c6180eb26de8b387e1814a5
Piyush K <piyushtirpude@gmail.com>
2026-04-03 21:05:52 +0530

    initail project setip done

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("hello world");
+})
+
+app.listen(3000,()=>{
+    console.log("App is active and listening on port no 3000");
+})
