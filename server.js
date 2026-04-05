bcf30b58aadb44962cccd8bd8a3315c6c964d13b
Piyush K <piyushtirpude@gmail.com>
2026-04-05 15:59:44 +0530

    added the gte,post,patch,delete apis

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const express = require("express");
-const app = express();
+const app = require("./src/app");
 
 app.get("/",(req,res)=>{
     res.send("hello world");
 })
 
 app.listen(3000,()=>{
-    console.log("App is active and listening on port no 3000");
+    console.log("App is active and listening on Port No:3000");
 })
