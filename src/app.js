e36b2709c52b3553a749e94ea8eeb7eb359e333e
Piyush K <piyushtirpude@gmail.com>
2026-04-08 22:55:54 +0530

    added multer for file uploads

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
-const NoteModel = require("../src/models/notes.model.js");
-
-
-
+const multer = require("multer");
+const uplaodFile = require("./services/storage.service.js");
 app.use(express.json());
+
+
+const upload = multer({storage: multer.memoryStorage()})
+
+app.post("/create-post", upload.single("image"), async(req,res)=>{
+    const result = await uplaodFile(req.file.buffer);
+})
+
 
 
 module.exports = app;
